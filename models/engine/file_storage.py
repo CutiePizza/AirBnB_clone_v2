a9735120aa9f64468602952bbbb56f3ede330b1b
AmineNeifer <amineneifer2000@gmail.com>
2020-03-30 14:17:13 +0100

    coolest task ever"

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,19 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        speci = {}
+        try:
+            for k, v in self.__objects.items():
+                if k.split('.')[0] == cls.__name__:
+                    speci[k] = v
+            return speci
+        except AttributeError:
+            return self.__objects
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +62,6 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        del self.__objects[type(obj).__name__ + '.' + obj.id]
