e936439e571039a0a68b76a2c7c40c7dda343dfa
CutiePizza <ines.chokri97@gmail.com>
2020-04-01 17:23:56 +0100

    pep8

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 import os
 
+
 class User(BaseModel, Base):
     """This is the class for user
     Attributes:
@@ -22,4 +23,3 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-
