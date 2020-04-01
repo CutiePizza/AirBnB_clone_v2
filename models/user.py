57f90b054383d8d1bc28e87a22e84782cf378535
AmineNeifer <amineneifer2000@gmail.com>
2020-04-01 16:58:01 +0100

    first name is nullable too

--- models/user.py
+++ models/user.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 import os
 
-
 class User(BaseModel, Base):
     """This is the class for user
     Attributes:
@@ -21,5 +20,6 @@ class User(BaseModel, Base):
         __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+
