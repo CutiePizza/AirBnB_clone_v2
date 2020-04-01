4ea387fd6d2aa80f01b9ac941ee6c95258ed7c93
AmineNeifer <amineneifer2000@gmail.com>
2020-04-01 16:40:48 +0100

    task 7

--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+import os
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -15,3 +17,9 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+    if (os.getenv('HBNB_TYPE_STORAGE') == "db"):
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=True)
