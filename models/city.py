22c2c35eec1f9bf6267dd8e4d4755fd5e540840e
CutiePizza <ines.chokri97@gmail.com>
2020-04-01 16:45:21 +0100

    add getter method in City

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """This is the city class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from models import storage
 
 
 class City(BaseModel, Base):
@@ -13,3 +14,15 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+
+    @property
+    def cities(self):
+        """
+        Getter attribute for cities
+        """
+        cities = storage.all(City)
+        dic = {}
+        for key, value in cities.items():
+            if value.state_id == self.id:
+                dic[key] = value
+        return dic
