a1055534a65bc81c8281515871a59c5c0e2896d2
AmineNeifer <amineneifer2000@gmail.com>
2020-04-02 13:55:32 +0100

    back to previous

--- models/amenity.py
+++ models/amenity.py
@@ -14,5 +14,4 @@ class Amenity(BaseModel, Base):
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
         "Place",
-        secondary=place_amenity,
-        back_populates='amenities')
+        secondary=place_amenity)
