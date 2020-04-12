fadedefccecf047bf01a09471143c146f4ade057
CutiePizza <ines.chokri97@gmail.com>
2020-04-12 23:55:16 +0100

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Task number one
+"""
+import os.path
+import tarfile
+import glob
+
+def do_pack():
+    """
+    File
+    """
+    source_dir = "./web_static"
+    dest_dir = "./versions"
+    archive_name = "./versions/web_static_20200414235157.tgz"
+    if not os.path.exists("./versions"):
+        os.makedirs("./versions")
+    try:
+        tar = tarfile.open(archive_name, "w:gz")
+        for file_name in glob.glob(os.path.join(source_dir, "*")):
+            tar.add(file_name, os.path.basename(file_name))
+        tar.close()
+        return (archive_name)
+    except:
+        return (None)
