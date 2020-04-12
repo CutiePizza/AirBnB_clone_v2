6dfcd3b5af7a524ae3cbb1c36ed105c3505d13e0
CutiePizza <ines.chokri97@gmail.com>
2020-04-12 21:20:56 +0100

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,6 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo servce nginx start
 fi
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 echo -e "<html>\n<head></head>\n<body>\nHolberton School\n</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
