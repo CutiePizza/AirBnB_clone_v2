97d6b3ff9ed906a8cd81a3a10e042b5e58d36497
CutiePizza <ines.chokri97@gmail.com>
2020-04-12 21:06:38 +0100

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,11 +11,11 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 echo -e "<html>\n<head></head>\n<body>\nHolberton School\n</body>\n</html>" | sudo tee -a /data/web_static/releases/test/index.html > /dev/null
-if [ -e /data/web_static/current ]; then
+if [ -e "/data/web_static/current" ]; then
 	sudo rm /data/web_static/current
 fi
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data/
 config="\\\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}"
 sudo sed -i "20i $config" /etc/nginx/sites-enabled/default
 sudo service nginx restart
