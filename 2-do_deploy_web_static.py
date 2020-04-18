4e6031e5c7efd264d9c2c6ce76a4f0e656fb564b
CutiePizza <ines.chokri97@gmail.com>
2020-04-18 16:06:16 +0100

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,53 +2,63 @@
 """
 Task number one
 """
-import datetime
-import os.path
-import tarfile
-import glob
-from fabric.api import env
-from fabric.operations import run, put, env
+from fabric.api import local, lcd, put, env, run
 
 
 env.hosts = ["35.243.214.205", "3.92.79.221"]
-env.user = ["ubuntu"]
 
 
 def do_deploy(archive_path):
     """
     Deploy
     """
-    if not os.path.exists(archive_path):
-        return (None)
+    archive_name = archive_path.split('/')[1]
+    name = archive_name.split('.')[0]
     try:
         put(archive_path, "/tmp/")
     except:
         return False
+
     try:
-        archive_name = archive_path.split('/')[1]
-        archive_without_ext = archive_name.split('.')[0]
-        run
-        ("tar --extract --file /tmp/{} -C /data/web_static/releases/{}".format(
+        path = '/data/web_static/releases/' + name
+        run('mkdir -p %s' % path)
+    except:
+        return False
+
+    try:
+        run('tar -xzf /tmp/%s -C /data/web_static/releases/%s/' % (
             archive_name,
-            archive_without_ext
+            name
             ))
     except:
         return False
+
     try:
-        archive_name = archive_path.split('/')[1]
-        run("rm /tmp/{}".format(archive_name))
+        run('rm /tmp/%s' % archive_name)
     except:
         return False
     try:
-        run("rm data/web_static/current")
+        run('sudo mv /data/web_static/releases/%s/web_static/*\
+                /data/web_static/releases/%s' % (
+                    name,
+                    name
+                    ))
     except:
         return False
+
     try:
-        archive_name = archive_path.split('/')[1]
-        archive_without_ext = archive_name.split('.')[0]
-        run
-        ("ln -s /data/web_static/releases/{} /data/web_static/current".format(
-            archive_without_ext))
+        run('rm -rf /data/web_static/releases/%s/web_static' % name)
+    except:
+        return False
+
+    try:
+        run('rm -rf /data/web_static/current')
+    except:
+        return False
+
+    try:
+        run('ln -s /data/web_static/releases/%s\
+                /data/web_static/current' % name)
     except:
         return False
     return True
