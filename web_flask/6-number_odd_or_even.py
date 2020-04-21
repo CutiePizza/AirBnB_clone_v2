34462071bf47287eb70a07dc21f6ceba4155273a
CutiePizza <ines.chokri97@gmail.com>
2020-04-22 00:18:52 +0100

    task 6 done

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+"""
+file five
+"""
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb")
+def hbnb_only():
+    return ("HBNB")
+
+
+@app.route("/c/<text>")
+def hbnb_custom(text):
+    return ("C %s" % text.replace('_', ' '))
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_route(text="is cool"):
+    return ("Python %s" % text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>")
+def is_number(n):
+    return ("%d is a number" % n)
+
+
+@app.route("/number_template/<int:n>")
+def displayPage_only_number(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def even_or_odd(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',
+            port=5000,
+            )
