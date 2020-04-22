ea341a2f7e31fd099384abf6229910e1ff5a4627
CutiePizza <ines.chokri97@gmail.com>
2020-04-22 20:58:53 +0100

    task 8 done

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+File 8
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """
+    teardown handling
+    """
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def all_citiess():
+    """
+    all cities
+    """
+    all_states = storage.all()
+    my_list = all_states.values()
+    return render_template('8-cities_by_states.html', my_list=my_list)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',
+            port=5000,
+            )
